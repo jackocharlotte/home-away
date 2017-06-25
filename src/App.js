5c1dfaf0b8cf8931f16c4c7bc728df0a3cd5831a
lottejackson <lottejackson@gmail.com>
2017-06-26 07:33:58 +1000

    Set up a project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Tile from './Tile.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Tile name="sdf"></Tile>
+      </div>
+    );
+  }
+}
+export default App;
